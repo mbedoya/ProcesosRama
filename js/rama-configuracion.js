1e5c1b325b731c28a714635cf4c204b35674fff4
mbedoya <mauricio.bedoya@gmail.com>
2017-03-23 12:48:14 -0500

    Ajustes: Se re lanzan los procesos detenidos y que no reportan actividad
    se cambia la configuración a archivo independiente
    Se adicionan campos de proveedor

---
+++ js/rama-configuracion.js
@@ -0,0 +1,10 @@
+//VARIABLES GLOBALES
+var urlConsulta_Konfirma = "http://52.24.84.124:4312/RamaJudicialKon/resources/Riesgos/RamaConsulta";
+var urlRegistro_Konfirma = "http://52.24.84.124:4312/RamaJudicialKon/resources/Riesgos/RamaInsert";
+var usuario_Konfirma = "KonfirmaRama";
+var clave_Konfirma = "z8uE9uEVI*fvV*$@k&O9";
+
+var urlConsultaProcesos = "http://procesos.ramajudicial.gov.co/consultaprocesos/";
+
+//Tiempo en segundos que debe transcurrir entre búsquedas de nuevos procesos pendientes por ejecución
+var tiempoEntreBusquedasProcesos = 60;
